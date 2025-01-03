ff470557933ae54034ec5eaad27faafc9de8fba5
Adele <160780354+adelemarcon@users.noreply.github.com>
2025-01-03 23:03:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ y = r*np.sin(theta)
 # En utilisant plotly
 
 fig = px.line(dict(x=x, y=y), x="x", y="y",
-              title = "Grâce à toi que mon coeur a pu se développer ❤", fontsize = 10
+              title = "Grâce à toi que mon coeur a pu se développer ❤", fontsize = 10,
               height=700,
               template='plotly_white',
               labels=dict(x="❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤", y="❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤")
